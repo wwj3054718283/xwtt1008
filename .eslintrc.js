8b5b8f60aefe05b89230f6185a3274dab3dfc2a5
纯肉攻城狮 <7547228+projectleo@user.noreply.gitee.com>
2022-03-15 13:04:50 +0800

    项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 配置eslint的引号规范，使用 单引号 不然就报错
+    "quotes":["error","single"]
   },
 };
