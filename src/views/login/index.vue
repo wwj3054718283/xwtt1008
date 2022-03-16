a9ec202395dfe267ecf9e3cd1f4306eaa7ee7687
纯肉攻城狮 <7547228+projectleo@user.noreply.gitee.com>
2022-03-16 22:37:00 +0800

    将token保存到本地和vue仓库

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
         <van-nav-bar title="登录" left-arrow />
 
         <!-- 登录表单 -->
-        <van-form @submit="onSubmit">
+        <van-form @submit="onSubmit" ref="loginForm">
             <!-- 手机号文本框 -->
             <van-field
                 v-model="user.mobile"
@@ -24,12 +24,25 @@
                 placeholder="请输入验证码"
             >
                 <i slot="left-icon" class="iconfont icon-yanzhengma"></i>
+                <!-- 右侧 发送短信 按钮 -->
                 <template #button>
+                    <!-- 倒计时器 -->
+                    <van-count-down
+                        v-if="isShowCount"
+                        :time="time"
+                        format="ss 秒"
+                        @finish="isShowCount = false"
+                    />
+
+                    <!-- 发送验证码按钮 -->
                     <van-button
+                        v-else
+                        native-type="button"
                         class="send-sms-btn"
                         round
                         size="small"
                         type="default"
+                        @click="sendSms"
                         >发送验证码</van-button
                     >
                 </template>
@@ -58,6 +71,8 @@ export default {
                 mobile: '',
                 // 验证码
                 code: 246810,
+                time: 60 * 1000,
+                isShowCount: false,
             },
             userRules: {
                 mobile: [
@@ -91,18 +106,33 @@ export default {
             });
             try {
                 // 2.发起登录 请求
-                let {data:res} = await login(this.user.mobile, this.user.code);
+                let { data: res } = await login(
+                    this.user.mobile,
+                    this.user.code
+                );
                 console.log(res);
                 // 3.轻提示 登录成功
                 this.$toast.success('登录成功咯~');
                 // 4.将 token保存到本地 和 vuex仓库
-                this.$store.commit('setUserToken', res.data)
+                this.$store.commit('setUserToken', res.data);
                 // 5.路由前进 跳转到#/my组件
                 // this.$router.push('/my')
             } catch (error) {
                 console.log(error);
                 // 6.轻提示 登录失败
                 this.$toast.fail('登录失败咯~');
+            }
+        },
+
+        // 发送短信验证码-------------------
+        async sendSms() {
+            // 1.使用 try catch 代替 成功 和 失败回调函数
+            try {
+                // 执行校验，并返回一个 Promise对象
+                await this.$refs.loginForm.validate('mobile');
+            } catch (error) {
+                // 校验失败，直接Return，后面代码就不执行了！
+                return console.log('校验失败了哦~！');
             }
         },
     },
